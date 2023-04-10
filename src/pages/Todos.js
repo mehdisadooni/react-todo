ef3a09a78b510f14013a1873c3428b632b6806b9
Mehdi sadooni <mdi.mehdi97@gmail.com>
2023-04-10 12:14:08 +0430

    install axios and use it

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -1,16 +1,36 @@
 import {useContext, useEffect} from "react";
 import TodoContext from "../context/TodoContext";
+import axios from "axios";
 
 const Todos = () => {
     const todoContext = useContext(TodoContext)
 
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(res => res.json())
-            .then(data => {
-                todoContext.dispatch({type: 'SET_TODOS', payload: data})
-            });
+
+        // fetch('https://jsonplaceholder.typicode.com/todos')
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         todoContext.dispatch({type: 'SET_TODOS', payload: data})
+        //     });
+        //
+        // axios.get('https://jsonplaceholder.typicode.com/todos')
+        //     .then(response => {
+        //         todoContext.dispatch({type: 'SET_TODOS', payload: response.data})
+        //     }).catch(error => {
+        //     console.log(error)
+        // })
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+                console.log(response.data)
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
+
+        fetchData()
+
     }, [])
 
     return (
